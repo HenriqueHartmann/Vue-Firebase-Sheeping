ba9708b7d464f54d16303e0c8722a9e60f4fdd08
HenriqueHartmann <alemaohartmann@gmail.com>
2021-08-15 04:21:11 -0300

    Refactor. Add active to list itens

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,28 +1,52 @@
 <template>
   <div>
-    <div v-if="!logged"></div>
-    <div v-else>
-      <div>
-        <nav id="menu" class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container">
-            <span @click="sendTo('main')" class="navbar-brand mb-0 h1" id="brand">
-              Sheeping
-            </span>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item"><span @click="sendTo('main')" class="nav-link">Home</span></li>
-                <li class="nav-item"><span @click="sendTo('profile')" class="nav-link">Profile</span></li>
-                <li class="nav-item"><span @click="signOut" class="nav-link">Sair</span></li>
-              </ul>
-            </div>
+    <div>
+      <nav id="menu" class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <span @click="sendTo('main')" class="navbar-brand mb-0 h1" id="brand">
+            Sheeping
+          </span>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <span
+                  @click="sendTo('main')"
+                  :class="{ active: active('main') }"
+                  class="nav-link"
+                  >Home</span
+                >
+              </li>
+              <li class="nav-item">
+                <span
+                  @click="sendTo('profile')"
+                  :class="{ active: active('profile') }"
+                  class="nav-link"
+                  >Profile</span
+                >
+              </li>
+              <li class="nav-item">
+                <span @click="signOut" class="nav-link">Sair</span>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-      <router-view />
+        </div>
+      </nav>
     </div>
+    <router-view />
   </div>
 </template>
 
@@ -32,26 +56,29 @@ import FButils from "@/mixins/firebaseUtils.js";
 export default {
   name: "Default",
   mixins: [FButils],
-  created: function () {
-    this.checkAuth();
-  },
   methods: {
     sendTo(route) {
       if (this.$route.name != route) {
         this.$router.push({ name: route });
       }
     },
+    active(route) {
+      if (this.$route.name == route) {
+        return true;
+      }
+      return false;
+    },
   },
 };
 </script>
 
 <style>
-  #brand:hover {
-    cursor: pointer;
-    color:lightslategray;
-  }
-  
-  #navbarNav ul li:hover{
-    cursor: pointer;
-  }
+#brand:hover {
+  cursor: pointer;
+  color: lightslategray;
+}
+
+#navbarNav ul li:hover {
+  cursor: pointer;
+}
 </style>
